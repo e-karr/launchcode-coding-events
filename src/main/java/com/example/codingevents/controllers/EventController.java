4d2f39bc1e077bd3adc4b968895a670bd9bbd315
Elizabeth Karr <e.karr93@icloud.com>
2023-07-31 14:51:54 -0500

    Create Event model; edit files to match starter code

--- src/main/java/com/example/codingevents/controllers/EventController.java
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -15,27 +15,24 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<HashMap<String, String>> events = new ArrayList<>();
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String description) {
-        HashMap<String, String> newEvent = new HashMap<>();
-        newEvent.put("eventName", eventName);
-        newEvent.put("description", description);
-        events.add(newEvent);
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(eventName);
         return "redirect:";
     }
 }
