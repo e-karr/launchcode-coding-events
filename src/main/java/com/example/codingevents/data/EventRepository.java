02a1a2ebe07148d7235e52505245d66e73a77540
Elizabeth Karr <e.karr93@icloud.com>
2023-08-26 19:03:36 -0500

    fix mysql dependency

--- src/main/java/com/example/codingevents/data/EventRepository.java
+++ src/main/java/com/example/codingevents/data/EventRepository.java
@@ -1,4 +1,9 @@
 package com.example.codingevents.data;
 
-public interface EventRepository {
+import com.example.codingevents.models.Event;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Integer> {
 }
