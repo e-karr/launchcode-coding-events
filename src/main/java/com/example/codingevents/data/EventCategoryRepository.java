b7a85e4364c4d3a89c140725a7ed7157f83894d3
Elizabeth Karr <e.karr93@icloud.com>
2023-08-30 19:31:59 -0500

    add repository annotation

--- src/main/java/com/example/codingevents/data/EventCategoryRepository.java
+++ src/main/java/com/example/codingevents/data/EventCategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.codingevents.data;
 
 import com.example.codingevents.models.EventCategory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventCategoryRepository extends CrudRepository<EventCategory, Integer> {
 }
