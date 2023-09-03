7b1a5ea4816c9a7e27d0e88e4f6c06432870c255
Elizabeth Karr <e.karr93@icloud.com>
2023-09-03 18:41:57 -0500

    follow video walkthrough

--- src/main/java/com/example/codingevents/models/Tag.java
+++ src/main/java/com/example/codingevents/models/Tag.java
@@ -1,8 +1,11 @@
 package com.example.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Tag extends AbstractEntity {
@@ -10,6 +13,9 @@ public class Tag extends AbstractEntity {
     @Size(min = 3, max = 25, message = "Name must be between 3 and 25 characters")
     @NotBlank(message = "Name is required")
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<>();
 
     public Tag() {}
 
@@ -23,6 +29,10 @@ public class Tag extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
