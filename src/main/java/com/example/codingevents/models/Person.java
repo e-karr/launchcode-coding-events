9e0cb16e2758448c93f88f54f226b7b4fdc11c38
Elizabeth Karr <e.karr93@icloud.com>
2023-09-05 19:23:00 -0500

    start design of person class

---
+++ src/main/java/com/example/codingevents/models/Person.java
@@ -0,0 +1,13 @@
+package com.example.codingevents.models;
+
+public class Person {
+    // username or email
+    // password (encrypted)
+    // name?
+    // list of Events they're interested in
+    // calendar?
+
+    // getters and setters
+    // make a calendar from list of events?
+    // maybe a Calendar class to make the calendar
+}
