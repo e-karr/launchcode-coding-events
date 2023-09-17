a48b5d3a28c7a060be95ef2551b6669e7f011138
Elizabeth Karr <e.karr93@icloud.com>
2023-09-17 13:23:53 -0500

    create User model

---
+++ src/main/java/com/example/codingevents/models/User.java
@@ -0,0 +1,25 @@
+package com.example.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String pwHash) {
+        this.username = username;
+        this.pwHash = pwHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
