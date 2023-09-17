db95fd479980dfc0e5d7a9291cd5ad32577d4f86
Elizabeth Karr <e.karr93@icloud.com>
2023-09-17 13:31:48 -0500

    update User model with hashing password

--- src/main/java/com/example/codingevents/models/User.java
+++ src/main/java/com/example/codingevents/models/User.java
@@ -1,5 +1,7 @@
 package com.example.codingevents.models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
@@ -12,14 +14,20 @@ public class User extends AbstractEntity {
     @NotNull
     private String pwHash;
 
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public User() {}
 
     public User(String username, String pwHash) {
         this.username = username;
-        this.pwHash = pwHash;
+        this.pwHash = encoder.encode(pwHash);
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
     }
 }
