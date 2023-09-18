e1ff9a12651a808d0bc024a65cb76802b138896b
Elizabeth Karr <e.karr93@icloud.com>
2023-09-18 18:38:33 -0500

    complete authentication filter and config

---
+++ src/main/java/com/example/codingevents/AuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.example.codingevents;
+
+import com.example.codingevents.controllers.AuthenticationController;
+import com.example.codingevents.data.UserRepository;
+import com.example.codingevents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter implements HandlerInterceptor {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.equals(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+}
